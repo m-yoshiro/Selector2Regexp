ca3b86d2c40d7ffd0b0304415f054ec1bc44b62d
m-yoshiro <massugu.massugu@gmail.com>
2019-12-08 22:31:31 +0900

    Add test

---
+++ test/generateRegexString.test.js
@@ -0,0 +1,26 @@
+const TestRunner = require("jest-runner");
+const generateRegexString = require("./../lib/generateRegexString");
+
+describe("generateRegexString()", () => {
+  it("throw an error without an argument", () => {
+    expect(() => {
+      generateRegexString()
+    }).toThrow(Error);
+
+    expect(() => {
+      generateRegexString({
+        name: 'example',
+        type: 'Selector'
+      })
+    }).toThrow(Error);
+  });
+
+  it('with className selector', () => {
+    const node = {
+      name: 'example',
+      type: 'ClassSelector',
+    };
+
+    expect(generateRegexString(node)).toBe("class=['\"]s?(example)s?['\"]");
+  })
+});

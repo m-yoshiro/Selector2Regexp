29f271cd1533451da9c3bed71df4c2f22bc418b1
m-yoshiro <massugu.massugu@gmail.com>
2020-04-05 16:53:24 +0900

    rename

---
+++ test/getSelector.test.ts
@@ -0,0 +1,15 @@
+const getSelectorNodes = require('../src/lib/getSelectorNodes');
+
+describe('getSelector', () => {
+  it('with className selector', () => {
+    const node = getSelectorNodes('.example')[0];
+    expect(node.name).toEqual('example');
+    expect(node.type).toEqual('ClassSelector');
+  });
+
+  it('with Id selector', () => {
+    const node = getSelectorNodes('#example')[0];
+    expect(node.name).toEqual('example');
+    expect(node.type).toEqual('IdSelector');
+  });
+});
---
+++ test/main.test.ts
@@ -0,0 +1,30 @@
+const cssSearchAdviser = require('../main');
+
+describe('initialize', () => {
+  it('throw an error without an argument', () => {
+    expect(() => {
+      cssSearchAdviser();
+    }).toThrow(Error);
+  });
+
+  it('with class selector', () => {
+    const regexStr = cssSearchAdviser('.example')[0];
+    const pattern = new RegExp(regexStr);
+
+    expect(regexStr).toBe('class=[\'"]s?(example)s?[\'"]');
+    expect(pattern.test('class="example"')).toBeTruthy();
+    expect(pattern.test('<div class="example">example</div>')).toBeTruthy();
+
+    expect(pattern.test('class="exampleTest"')).toBeFalsy();
+    expect(pattern.test('<div class="exampleTest">example</div>')).toBeFalsy();
+  });
+
+  it('with id selector', () => {
+    const regexStr = cssSearchAdviser('#example')[0];
+    const pattern = new RegExp(regexStr);
+
+    expect(regexStr).toBe('id=[\'"]s?(example)s?[\'"]');
+    expect(pattern.test('id="example"')).toBeTruthy();
+    expect(pattern.test('id="exampleTest"')).toBeFalsy();
+  });
+});

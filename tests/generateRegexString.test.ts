aa47e8ad0b4985d8b00962d220b10c3a8917c27e
m-yoshiro <massugu.massugu@gmail.com>
2020-05-02 12:29:24 +0900

    fix generateRegexStrings test

--- tests/generateRegexString.test.ts
+++ tests/generateRegexString.test.ts
@@ -1,31 +1,11 @@
 import generateRegexString from '../src/lib/generateRegexString';
 import csstree from 'css-tree';
 
-const ast = csstree.parse('.example', {
-  context: 'selector',
-  onParseError: (error) => {
-    console.log(error.message);
-  },
-});
-
 describe('generateRegexString()', () => {
-  it('throw an error without an argument', () => {
-    expect(() => {
-      generateRegexString();
-    }).toThrow(Error);
-
-    expect(() => {
-      generateRegexString({
-        name: 'example',
-        type: 'Selector',
-      });
-    }).toThrow(Error);
-  });
-
   it('with className selector', () => {
-    const node = {
-      name: 'example',
+    const node: csstree.ClassSelector = {
       type: 'ClassSelector',
+      name: 'example',
     };
 
     expect(generateRegexString(node)).toBe('class=[\'"]s?(example)s?[\'"]');

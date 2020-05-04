067cd918e94ae970b0ddde90bd3744a276d529d5
m-yoshiro <massugu.massugu@gmail.com>
2020-05-04 11:33:18 +0900

    implement single class and id selector

--- src/lib/convertNodeToRegex.ts
+++ src/lib/convertNodeToRegex.ts
@@ -1,22 +1,80 @@
-import { IdOrClassSelector } from '../../types';
 import csstree from 'css-tree';
 
-const translation = {
-  ClassSelector: 'class',
-  IdSelector: 'id',
+const START_OF_BRACKET = '<\\s*';
+const END_OF_BRACKET = '\\s*>';
+const TYPE_NAME = '\\w+';
+const CLASS_ATTRIBUTE = 'class';
+const ANY_VALUE = '\\w*';
+const ANY_ID = '\\w*';
+const ID_ATTRIBUTE = 'id';
+const ATTRIBUTE_SEPARATOR = '\\s+';
+const SPACE_BETWEEN_ELEMENT = '\\s*';
+const QUOTE = '[\'"]';
+const BEFORE_ATTRIBUTE = `(?<!\\w)`; // ES2018
+const AFTER_ATTRIBUTE = '(?!\\w)';
+
+const attributeRegexp = <T extends string>(attribute: string, value: T | T[]) => {
+  const valueRegexp = (value: T | T[]) => {
+    const valueString = Array.isArray(value) ? value.join('|') : value;
+    const n = Array.isArray(value) ? `{${value.length}}` : '';
+
+    return `${ANY_VALUE}${SPACE_BETWEEN_ELEMENT}${BEFORE_ATTRIBUTE}(${valueString})${AFTER_ATTRIBUTE}${SPACE_BETWEEN_ELEMENT}${ANY_VALUE}${n}`;
+  };
+
+  return `${attribute}=${QUOTE}${valueRegexp(value)}${QUOTE}`;
 };
 
-export default (node: csstree.Selector) => {
-  if (!node) {
+export default (selector: csstree.Selector) => {
+  if (!selector) {
     throw new Error('1 argument required, but only 0 present.');
   }
 
-  if (!(node.type in translation)) {
-    throw new Error(`Bad node type ${node.type} for 'generateRegexString'. should be ${Object.keys(translation).join(', ')}`);
+  if (selector.type !== 'Selector') {
+    throw new Error(`Bad node type ${selector.type} for 'generateRegexString'.`);
   }
 
-  const selector = node.name;
-  const attribute = translation[node.type];
+  const result: {
+    type: 'ClassSelector' | 'IdSelector' | 'WhiteSpace';
+    value: string;
+  }[] = [];
 
-  return `${attribute}=(['"])\s?(${selector})\s?\1`;
+  csstree.walk(selector, (node) => {
+    switch (node.type) {
+      case 'ClassSelector':
+        result.push({
+          type: node.type,
+          value: attributeRegexp(CLASS_ATTRIBUTE, node.name),
+        });
+        break;
+
+      case 'IdSelector':
+        result.push({
+          type: node.type,
+          value: attributeRegexp(ID_ATTRIBUTE, node.name),
+        });
+        break;
+
+      case 'WhiteSpace':
+        result.push({
+          type: node.type,
+          value: END_OF_BRACKET + SPACE_BETWEEN_ELEMENT + START_OF_BRACKET + TYPE_NAME,
+        });
+        break;
+
+      case 'Combinator':
+        if (node.name === '>' || node.name === '+' || node.name === '~') {
+          throw new Error(`Combinator "${node.name}" is not supported.`);
+        }
+        break;
+
+      case 'PseudoElementSelector':
+        throw new Error('Pseudo-elements "before" or "after" is not supported.');
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  return result.map((selector) => selector.value).join('');
 };
--- tests/convertNodeToRegex.test.ts
+++ tests/convertNodeToRegex.test.ts
@@ -1,13 +1,70 @@
 import convertNodeToRegex from '../src/lib/convertNodeToRegex';
 import csstree from 'css-tree';
 
-describe('generateRegexString()', () => {
-  it('with className selector', () => {
-    const node: csstree.ClassSelector = {
-      type: 'ClassSelector',
-      name: 'example',
-    };
+const selector = (str: string) =>
+  csstree.parse(str, {
+    context: 'selector',
+  }) as csstree.Selector;
 
-    expect(convertNodeToRegex(node)).toBe('class=[\'"]s?(example)s?[\'"]');
+describe('generateRegexString()', () => {
+  describe('given an ClassName selector', () => {
+    it('Should be equal', () => {
+      expect(convertNodeToRegex(selector('.example'))).toBe('class=[\'"]\\w*\\s*(?<!\\w)(example)(?!\\w)\\s*\\w*[\'"]');
+    });
+
+    it('Find an given className in HTML', () => {
+      expect(new RegExp(convertNodeToRegex(selector('.example'))).test(`<div class="example"></div>`)).toBeTruthy();
+      expect(new RegExp(convertNodeToRegex(selector('.example'))).test(`<div class="left example"></div>`)).toBeTruthy();
+      expect(new RegExp(convertNodeToRegex(selector('.example'))).test(`<div class="left example right"></div>`)).toBeTruthy();
+    });
+
+    it('Fail to find an given className in HTML', () => {
+      expect(new RegExp(convertNodeToRegex(selector('.example'))).test(`<div class="xample"></div>`)).toBeFalsy();
+      expect(new RegExp(convertNodeToRegex(selector('.example'))).test(`<div class="leftexample"></div>`)).toBeFalsy();
+      expect(new RegExp(convertNodeToRegex(selector('.example'))).test(`<div class="left exampleright"></div>`)).toBeFalsy();
+    });
+  });
+
+  describe('given an ID selector', () => {
+    it('Should be equal', () => {
+      expect(convertNodeToRegex(selector('#app'))).toBe('id=[\'"]\\w*\\s*(app)\\s*\\w*[\'"]');
+    });
+
+    it('Find an given ID in HTML', () => {
+      expect(new RegExp(convertNodeToRegex(selector('#app'))).test(`<div id="app"></div>`)).toBeTruthy();
+      expect(new RegExp(convertNodeToRegex(selector('#app'))).test(`<div id="left app"></div>`)).toBeTruthy();
+      expect(new RegExp(convertNodeToRegex(selector('#app'))).test(`<div id="left app right"></div>`)).toBeTruthy();
+    });
+  });
+
+  describe('given an Attribute selector', () => {
+    it('Should be equal', () => {
+      expect(convertNodeToRegex(selector('[class=button]'))).toBe('class=[\'"]\\s?(button)\\s?[\'"]');
+    });
+  });
+
+  describe('given an Descendant – Whitespace – combinator', () => {
+    it('with Descendant – Whitespace – Combinator', () => {
+      expect(convertNodeToRegex(selector('.example .child'))).toBe('class=[\'"]\\s?(example)\\s?[\'"]\\s*.*>\\s*<\\s*(\\w+)\\s*/>');
+    });
+
+    it('Descendant valid regex', () => {
+      const html = `<div class="example"><div class="child"></div></div>`;
+      console.log(html);
+      expect(new RegExp(convertNodeToRegex(selector('.example .child'))).test(html)).toBeTruthy();
+    });
+  });
+
+  describe('given an unsupported selector', () => {
+    it('with ">", "+" and "~" Combinator throw Error', () => {
+      expect(() => convertNodeToRegex(selector('.example > .child'))).toThrowError('Combinator ">" is not supported.');
+      expect(() => convertNodeToRegex(selector('.example + .adjacentSibling'))).toThrowError('Combinator "+" is not supported.');
+      expect(() => convertNodeToRegex(selector('.example ~ .sibling'))).toThrowError('Combinator "~" is not supported.');
+    });
+
+    it('with Pseudo-elements thtow Error', () => {
+      expect(() => convertNodeToRegex(selector('.example::before'))).toThrowError('Pseudo-elements "before" or "after" is not supported.');
+      expect(() => convertNodeToRegex(selector('.example::after'))).toThrowError('Pseudo-elements "before" or "after" is not supported.');
+    });
   });
 });

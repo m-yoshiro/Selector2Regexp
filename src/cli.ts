6075f1d1d2663845167eb3b346a96f394fa3d024
m-yoshiro <massugu.massugu@gmail.com>
2020-05-05 16:06:12 +0900

    npm link

--- src/cli.ts
+++ src/cli.ts
@@ -1,27 +1,28 @@
 import selector2Regexp from './index';
 import yargs from 'yargs';
-import { version } from '../package.json';
 
-const argv = yargs
-  .usage(
-    `
+export default () => {
+  const argv = yargs
+    .usage(
+      `
 Usage:
   $0 [CSS Selector]
 `
-  )
-  .example('$0 ".example"', 'Basic usage')
-  .demandCommand(1)
-  .help('h')
-  .alias('h', 'help')
-  .version(version)
-  .alias('v', 'version').argv;
+    )
+    .example('$0 ".example"', 'Basic usage')
+    .demandCommand(1)
+    .help('h')
+    .alias('h', 'help')
+    .version(require('../package.json').version)
+    .alias('v', 'version').argv;
 
-const input = argv._;
+  const input = argv._;
 
-console.log(argv);
+  console.log(argv);
 
-if (input.length > 1) {
-  process.exit(1);
-}
+  if (input.length > 1) {
+    process.exit(1);
+  }
 
-console.log(selector2Regexp(input[0]));
+  console.log(selector2Regexp(input[0]));
+};

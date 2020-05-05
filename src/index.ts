888266b4b017a69cf2c13a0a649460f0b15689de
m-yoshiro <massugu.massugu@gmail.com>
2020-05-05 23:30:05 +0900

    convertStringToSelector -> parse

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import convertStringToSelector from './lib/convertStringToSelector';
+import parse from './lib/parse';
 import convertNodeToRegex from './lib/convertNodeToRegex';
 import { CSSSelectorString } from '../types';
 
@@ -6,7 +6,7 @@ export default function (data: CSSSelectorString) {
   if (!data) {
     throw new Error('1 argument required, but only 0 present.');
   }
-  const selectorNode = convertStringToSelector(data);
+  const selectorNode = parse(data);
 
   return convertNodeToRegex(selectorNode);
 }
---
+++ tests/parse.test.ts
@@ -0,0 +1,16 @@
+import parse from '../src/lib/convertStringToSelector';
+import csstree from 'css-tree';
+
+describe('convertToNodes', () => {
+  it('with class selector', () => {
+    expect(parse('.example')).toStrictEqual(csstree.parse('.example', { context: 'selector' }));
+  });
+
+  it('with ID selector', () => {
+    expect(parse('#example')).toStrictEqual(csstree.parse('#example', { context: 'selector' }));
+  });
+
+  it('with multiple selector', () => {
+    expect(parse('.example .child')).toStrictEqual(csstree.parse('.example .child', { context: 'selector' }));
+  });
+});

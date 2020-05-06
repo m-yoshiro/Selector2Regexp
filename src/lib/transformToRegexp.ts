bb5028d81f27926056d57deffc85d14a9f5538ad
m-yoshiro <massugu.massugu@gmail.com>
2020-05-06 12:11:43 +0900

    Separate visitor

--- src/lib/transformToRegexp.ts
+++ src/lib/transformToRegexp.ts
@@ -1,105 +1,44 @@
 import csstree from 'css-tree';
+import { visitor, Visitor } from './visitor';
 
-const START_OF_BRACKET = '<\\s*';
-const END_OF_BRACKET = '\\s*>';
-const TYPE_NAME = '\\w+';
-const CLASS_ATTRIBUTE = 'class';
-const ANY_VALUE = '\\w*';
-const ID_ATTRIBUTE = 'id';
-const ATTRIBUTE_SEPARATOR = '\\s+';
-const SPACE_BETWEEN_ELEMENT = '\\s*';
-const QUOTE = '[\'"]';
-const BEFORE_ATTRIBUTE = `(?<!\\w)`; // ES2018
-const AFTER_ATTRIBUTE = '(?!\\w)';
-
-const attributeRegexp = <T extends string>(attribute: string, value: T | T[] | null) => {
-  if (!value) {
-    return `${attribute}`;
-  }
-
-  const valueRegexp = (value: T | T[]) => {
-    const valueString = Array.isArray(value) ? value.join('|') : value;
-    const n = Array.isArray(value) ? `{${value.length}}` : '';
-
-    return `${ANY_VALUE}${SPACE_BETWEEN_ELEMENT}${BEFORE_ATTRIBUTE}(${valueString})${AFTER_ATTRIBUTE}${SPACE_BETWEEN_ELEMENT}${ANY_VALUE}${n}`;
-  };
-
-  return `${attribute}=${QUOTE}${valueRegexp(value)}${QUOTE}`;
-};
-
-const openingTagRegexp = (type: string, attribute?: string) => {
-  return START_OF_BRACKET + type + END_OF_BRACKET;
-};
-
-const closingTagRegexp = (type: string) => {
-  return START_OF_BRACKET + '/' + type + END_OF_BRACKET;
-};
-
-export default (selector: csstree.Selector) => {
-  if (!selector) {
-    throw new Error('1 argument required, but only 0 present.');
-  }
-
+export default function (selector: csstree.Selector) {
   if (selector.type !== 'Selector') {
     throw new Error(`Bad node type ${selector.type} for 'generateRegexString'.`);
   }
 
-  const result: {
-    type: 'ClassSelector' | 'IdSelector' | 'AttributeSelector' | 'WhiteSpace' | 'TypeSelector';
-    value: string;
-  }[] = [];
-
+  const result: unknown[] = [];
   csstree.walk(selector, (node) => {
+    // TOFIX:
+    // if (node.type in visitor) {
+    //   visitor[node.type](node);
+    // }
+
     switch (node.type) {
       case 'ClassSelector':
-        result.push({
-          type: node.type,
-          value: attributeRegexp(CLASS_ATTRIBUTE, node.name),
-        });
+        result.push(visitor[node.type](node));
         break;
-
       case 'IdSelector':
-        result.push({
-          type: node.type,
-          value: attributeRegexp(ID_ATTRIBUTE, node.name),
-        });
+        result.push(visitor[node.type](node));
         break;
-
-      case 'AttributeSelector':
-        result.push({
-          type: node.type,
-          value: attributeRegexp(node.name.name, (node.value as csstree.Identifier).name),
-        });
-        break;
-
       case 'TypeSelector':
-        result.push({
-          type: node.type,
-          value: openingTagRegexp(node.name),
-        });
+        result.push(visitor[node.type](node));
         break;
-
       case 'WhiteSpace':
-        result.push({
-          type: node.type,
-          value: END_OF_BRACKET + SPACE_BETWEEN_ELEMENT + START_OF_BRACKET + TYPE_NAME + ATTRIBUTE_SEPARATOR,
-        });
+        result.push(visitor[node.type](node));
         break;
-
+      case 'AttributeSelector':
+        result.push(visitor[node.type](node));
+        break;
       case 'Combinator':
-        if (node.name === '>' || node.name === '+' || node.name === '~') {
-          throw new Error(`Combinator "${node.name}" is not supported.`);
-        }
+        result.push(visitor[node.type](node));
         break;
-
       case 'PseudoElementSelector':
-        throw new Error('Pseudo-elements "before" or "after" is not supported.');
+        result.push(visitor[node.type](node));
         break;
-
       default:
         break;
     }
   });
 
-  return result.map((selector) => selector.value).join('');
-};
+  return result.join('');
+}
---
+++ src/lib/visitor.ts
@@ -0,0 +1,80 @@
+import csstree, { PseudoElementSelector } from 'css-tree';
+import { CssSearchSelector } from '../../types';
+
+const START_OF_BRACKET = '<\\s*';
+const END_OF_BRACKET = '\\s*>';
+const TYPE_NAME = '\\w+';
+const CLASS_ATTRIBUTE = 'class';
+const ANY_VALUE = '\\w*';
+const ID_ATTRIBUTE = 'id';
+const ATTRIBUTE_SEPARATOR = '\\s+';
+const SPACE_BETWEEN_ELEMENT = '\\s*';
+const QUOTE = '[\'"]';
+const BEFORE_ATTRIBUTE = `(?<!\\w)`; // ES2018
+const AFTER_ATTRIBUTE = '(?!\\w)';
+
+type SelectorRegexpString = string;
+type NoSupport = Error | void;
+
+export type Visitor = {
+  ClassSelector: (node: csstree.ClassSelector) => SelectorRegexpString;
+  IdSelector: (node: csstree.IdSelector) => SelectorRegexpString;
+  AttributeSelector: (node: csstree.AttributeSelector) => SelectorRegexpString;
+  WhiteSpace: (node: csstree.WhiteSpace) => SelectorRegexpString;
+  TypeSelector: (node: csstree.TypeSelector) => SelectorRegexpString;
+  Combinator: (node: csstree.Combinator) => NoSupport;
+  PseudoElementSelector: (node: PseudoElementSelector) => NoSupport;
+};
+
+const attributeRegexp = <T extends string>(attribute: string, value: T | T[] | null) => {
+  if (!value) {
+    return `${attribute}`;
+  }
+
+  const valueRegexp = (value: T | T[]) => {
+    const valueString = Array.isArray(value) ? value.join('|') : value;
+    const n = Array.isArray(value) ? `{${value.length}}` : '';
+
+    return `${ANY_VALUE}${SPACE_BETWEEN_ELEMENT}${BEFORE_ATTRIBUTE}(${valueString})${AFTER_ATTRIBUTE}${SPACE_BETWEEN_ELEMENT}${ANY_VALUE}${n}`;
+  };
+
+  return `${attribute}=${QUOTE}${valueRegexp(value)}${QUOTE}`;
+};
+
+const openingTagRegexp = (type: string, attribute?: string) => {
+  return START_OF_BRACKET + type + END_OF_BRACKET;
+};
+
+const closingTagRegexp = (type: string) => {
+  return START_OF_BRACKET + '/' + type + END_OF_BRACKET;
+};
+
+export const visitor: Visitor = {
+  ClassSelector(node) {
+    return attributeRegexp(CLASS_ATTRIBUTE, node.name);
+  },
+
+  IdSelector(node) {
+    return attributeRegexp(ID_ATTRIBUTE, node.name);
+  },
+
+  AttributeSelector(node) {
+    return attributeRegexp(node.name.name, (node.value as csstree.Identifier).name);
+  },
+
+  TypeSelector(node) {
+    return openingTagRegexp(node.name);
+  },
+
+  WhiteSpace(node) {
+    return END_OF_BRACKET + SPACE_BETWEEN_ELEMENT + START_OF_BRACKET + TYPE_NAME + ATTRIBUTE_SEPARATOR;
+  },
+
+  Combinator(node) {
+    throw new Error(`Combinator "${node.name}" is not supported.`);
+  },
+
+  PseudoElementSelector(node) {
+    throw new Error('Pseudo-elements "before" or "after" is not supported.');
+  },
+};

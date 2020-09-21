f822de4658cafb4b3d3e92f84711df807664c3fa
m-yoshiro <massugu.massugu@gmail.com>
2020-09-21 17:55:29 +0900

    Assembling modules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { parse } from './lib/parse';
-import { transformToRegexp } from './lib/transformToRegexp';
+import { generate } from './lib/generate/generate';
+import { convert } from './lib/convert/convert';
+// import { transformToRegexp } from './lib/transformToRegexp';
 import { CSSSelectorString } from '../types';
 
 export default function (data: CSSSelectorString) {
@@ -7,6 +9,8 @@ export default function (data: CSSSelectorString) {
     throw new Error('1 argument required, but only 0 present.');
   }
   const selectorNode = parse(data);
+  const ast = convert(selectorNode);
 
-  return transformToRegexp(selectorNode);
+  return generate(ast);
+  // return transformToRegexp(selectorNode);
 }

d7b85e914d4795d8dfefb2e3aece701df09df96d
m-yoshiro <massugu.massugu@gmail.com>
2020-09-26 07:53:41 +0900

    Simplified elementGenerate

--- src/lib/generate/elementGenerate.ts
+++ src/lib/generate/elementGenerate.ts
@@ -1,24 +1,20 @@
 import { START_OF_BRACKET, END_OF_BRACKET, ANY_TYPE_NAME, ATTRIBUTE_SEPARATOR } from '../utils/definitions';
 
-export const elementTemplate = (value: { type?: string; attributes?: string }) => {
-  if (value.type && !value.attributes) {
-    return openingTag(value.type);
+const attributesCompile = (attrs: string[]) => {
+  if (attrs.length >= 2) {
+    return attrs.join('|') + `{${attrs.length}}`;
+  } else {
+    return attrs.join('');
+  }
+};
+
+export const elementTemplate = (value: { type: string; attributes?: string[] }) => {
+  if (!value.attributes || value.attributes.length < 1) {
+    return START_OF_BRACKET + `(${value.type})` + '\\s*.*?' + END_OF_BRACKET;
   }
 
   const type = value.type || ANY_TYPE_NAME;
-  const attributes = value.attributes || '';
+  const attributes = value.attributes;
 
-  return START_OF_BRACKET + type + ATTRIBUTE_SEPARATOR + attributes + END_OF_BRACKET;
-};
-
-export const openingTagNoClosing = (type: string) => {
-  return START_OF_BRACKET + `(${type})` + '\\s*.*?';
-};
-
-export const openingTag = (type: string) => {
-  return openingTagNoClosing(type) + END_OF_BRACKET;
-};
-
-export const closingTag = (type: string) => {
-  return START_OF_BRACKET + '/' + type + END_OF_BRACKET;
+  return START_OF_BRACKET + type + ATTRIBUTE_SEPARATOR + attributesCompile(attributes) + END_OF_BRACKET;
 };

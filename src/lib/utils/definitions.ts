55297e9c00a5146ee91dc080eacc668955bd9243
m-yoshiro <massugu.massugu@gmail.com>
2020-09-26 07:54:17 +0900

    Added a definition

--- src/lib/utils/definitions.ts
+++ src/lib/utils/definitions.ts
@@ -3,6 +3,8 @@ export const END_OF_BRACKET = '\\s*>';
 export const ANY_TYPE_NAME = '\\w+';
 export const CLASS_ATTRIBUTE = 'class';
 export const ANY_VALUE = '\\w*';
+export const ANY_ATTRIBUTE_NAME = '[\\w-]*';
+export const ANY_ATTRIBUTE_VALUE = '[\\w-=:]*';
 export const ID_ATTRIBUTE = 'id';
 export const ATTRIBUTE_SEPARATOR = '\\s+';
 export const SPACE_BETWEEN_ELEMENT = '\\s*';
@@ -12,3 +14,5 @@ export const BEFORE_ATTRIBUTE = '(?<!\\w)'; // ES2018
 export const AFTER_ATTRIBUTE = '(?!\\w)';
 export const ANY_OPENING_TAG = '<.*>';
 export const ANY_CLOSING_TAG = '</.*>';
+
+export const ANY_ATTRIBUTE = `${ANY_ATTRIBUTE_NAME}(?:=${QUOTE}${ANY_ATTRIBUTE_VALUE}${QUOTE})?`;

3b2327b81a03b348686b193466eac0b4fcf01562
m-yoshiro <massugu.massugu@gmail.com>
2020-09-27 09:38:00 +0900

    separate attributeRegexTemplate

--- src/lib/generate/idGenerate.ts
+++ src/lib/generate/idGenerate.ts
@@ -1,19 +1,9 @@
 import { ANY_VALUE, QUOTE, BEFORE_ATTRIBUTE, AFTER_ATTRIBUTE, SPACE_BETWEEN_VALUE } from '../utils/definitions';
-
-const wrapQuate = (value: string) => {
-  return `${QUOTE}${value}${QUOTE}`;
-};
-
-export const attributeRegexpTemplate = (name: string, value?: string) => {
-  if (!value) {
-    return name;
-  }
-  return `${name}=${wrapQuate(value)}`;
-};
+import { attributeRegexpTemplate } from './attributeRegexTemplate';
 
 export const idToRegexp = (id: string) => {
   const attrName = 'id';
   const valueRegex = ANY_VALUE + SPACE_BETWEEN_VALUE + BEFORE_ATTRIBUTE + `(${id})` + AFTER_ATTRIBUTE + SPACE_BETWEEN_VALUE + ANY_VALUE;
 
-  return attributeRegexpTemplate(attrName, valueRegex);
+  return attributeRegexpTemplate(attrName, `(${QUOTE}${valueRegex}${QUOTE})`);
 };

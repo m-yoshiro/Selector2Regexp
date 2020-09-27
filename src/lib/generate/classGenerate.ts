0938b4f80a99a679f9bac852212d36f1f346eaa6
m-yoshiro <massugu.massugu@gmail.com>
2020-09-27 09:51:42 +0900

    simplified

--- src/lib/generate/classGenerate.ts
+++ src/lib/generate/classGenerate.ts
@@ -2,18 +2,11 @@ import { ANY_VALUE, QUOTE, BEFORE_ATTRIBUTE, AFTER_ATTRIBUTE, SPACE_BETWEEN_VALU
 import { attributeRegexpTemplate } from './attributeRegexTemplate';
 
 export const classToRegexp = (classList: string[]) => {
-  const isMultiple = classList.length >= 2;
-  let valueString: string;
-
-  if (isMultiple) {
-    valueString = classList
-      .map((classItem) => {
-        return `(?=(.*[\\s'"]${classItem}[\\s'"]))`;
-      })
-      .join('');
-  } else {
-    valueString = `(?=(.*[\\s'"]${classList[0]}[\\s'"]))`;
-  }
+  const valueString = classList
+    .map((classItem) => {
+      return `(?=(.*[\\s'"]${classItem}[\\s'"]))`;
+    })
+    .join('');
 
   const attrName = 'class';
   const valueRegex = `(?=${QUOTE})(${valueString}.*)(?=${QUOTE})`;
